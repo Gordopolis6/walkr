29704269b5753a06a983a9542e609ec6de22a8d1
Emily Rosenbery <emily.rosenbery@mnsu.edu>
2017-04-17 18:34:09 +0000

    Start cucumber, nowhere near finished :)

--- Walkr/db/schema.rb
+++ Walkr/db/schema.rb
@@ -13,11 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20170410203322) do
 
-  create_table "moviegoers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
---
+++ Walkr/features/brandpage.feature
@@ -0,0 +1,7 @@
+Feature: "Brand" page
+  
+  As An App user,
+  I want a brand page,
+  So that I know what walkr is and what it does
+  
+Scenario: Brand page
